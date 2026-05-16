c2837dd429ef5c1e31fd01887b29853ced00ead0
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-05-16 23:53:00 +0300

    login and register styled

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -18,36 +18,47 @@ function Login ({username, password, setPassword, setUsername, onLogin}) {
 
     return(
         <>
-        <div>
-            <div>
-                <h1>Login to</h1>
-            </div>
-            <div>
-                <h1>Food4U</h1>
-            </div>
-            <form onSubmit={ handleSubmit }>
-                <h2>Login</h2>
-                <label htmlFor="username">Username:</label><br />
-                <input 
-                type="text"
-                placeholder="Type your username..."
-                value={username}
-                onChange={handleChange1}
-                required
-                 /> <br />
-                <label htmlFor="password">Password:</label><br />
-                <input 
-                type="password"
-                placeholder="Type your password..."
-                value={password}
-                onChange={handleChange2}
-                required
-                 /> <br />
-                <button type="submit">Login</button>
-            </form>
-            <div>
-                <h2>Are you new to Food4U?</h2>
-                <Link to="/Register">Create an account</Link>   
+        <div className="min-h-screen bg-linear-to-b from-yellow-300 via-yellow-500 to-orange-600 flex items-center justify-center">
+            <div className="bg-white/50 backdrop-blur-md border-white/60 rounded-4xl shadow-2xl/35 p-8 md:p-14 w-full max-w-5xl">
+                <h1 className="text-3xl text-center">
+                    WELCOME TO
+                </h1>
+                <h1 className="text-6xl text-center font-food font-extrabold mb-4">
+                    Food4U
+                </h1>
+                <form onSubmit={ handleSubmit } className="rounded-xl flex items-center justify-center h-55 m-7">
+                    <div>
+                        <h2 className="underline text-5xl font-semibold">
+                            LOG-IN
+                        </h2>
+                        <label htmlFor="username" className="text-3xl">Username:</label>
+                        <input 
+                        type="text"
+                        placeholder="Type your username..."
+                        value={username}
+                        onChange={handleChange1}
+                        required
+                        className="border border-slate-300 rounded-md px-3 py-2 focus:bg-white w-full focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 m-2 "
+                        /> 
+                        <label htmlFor="password" className="text-3xl">Password:</label>
+                        <input 
+                        type="password"
+                        placeholder="Type your password..."
+                        value={password}
+                        onChange={handleChange2}
+                        required
+                        className="border border-slate-300 rounded-md px-3 py-2 focus:bg-white w-full focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 m-2 "
+                        /> 
+                        <button type="submit"
+                        className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 focus:bg-white/25 px-4 rounded m-2">
+                        Login
+                        </button>
+                    </div>
+                </form>
+                <div className="flex flex-col items-center justify-center py-">
+                    <h2>Are you new to Food4U?</h2>
+                    <Link to="/Register" className="underline decoration-transparent transition duration-300 hover:decoration-inherit text-red-500">Create an account</Link>   
+                </div>
             </div>
         </div>
         </>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 
 function Register ({username, password, setPassword, setUsername, onRegister}) {
     function handleSubmit (e) {
@@ -15,33 +16,39 @@ function Register ({username, password, setPassword, setUsername, onRegister}) {
 
     return(
         <>
-        <div>
-            <div>
-                <h1>Welcome to</h1>
+        <div className="min-h-screen bg-linear-to-b from-yellow-300 via-yellow-500 to-orange-600 flex items-center justify-center">
+            <div className="bg-white/50 backdrop-blur-md border-white/60 rounded-4xl shadow-2xl/35 p-8 md:p-14 w-full max-w-5xl">
+                <h1 className="text-3xl text-center">Welcome to</h1>
+                <h1 className="text-6xl text-center font-food font-extrabold mb-8">Food4U</h1>
+                <form onSubmit={ handleSubmit } className="rounded-xl flex items-center justify-center h-55 m-7">
+                    <div>
+                        <h1 className="underline text-5xl font-semibold">Register an account</h1>
+                        <label htmlFor="name" className="text-3xl">Username:</label>
+                        <input 
+                        type="text"
+                        placeholder="Type your name..."
+                        value={username}
+                        onChange={handleChange1}
+                        required
+                        className="border border-slate-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 m-2 focus:bg-white"
+                        /> 
+                        <label htmlFor="password" className="text-3xl">Password:</label>
+                        <input 
+                        type="text"
+                        placeholder="Type your password..."
+                        value={password}
+                        onChange={handleChange2}
+                        required
+                        className="border border-slate-300 rounded-md px-3 py-2 w-full focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 m-2 focus:bg-white"
+                        /> 
+                        <button type="submit" className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded m-2">Register</button>
+                    </div>
+                </form>
+                <div className="flex flex-col items-center justify-center py-">
+                    <h2>Already a member?</h2>
+                    <Link to="/" className="underline decoration-transparent transition duration-300 hover:decoration-inherit text-red-500">Login</Link>   
+                </div>
             </div>
-            <div>
-                <h1>Food4U</h1>
-            </div>
-            <form onSubmit={ handleSubmit }>
-                <h1>Register an account</h1>
-                <label htmlFor="name">Username:</label><br />
-                <input 
-                type="text"
-                placeholder="Type your name..."
-                value={username}
-                onChange={handleChange1}
-                required
-                 /> <br />
-                <label htmlFor="password">Password:</label><br />
-                <input 
-                type="text"
-                placeholder="Type your password..."
-                value={password}
-                onChange={handleChange2}
-                required
-                 /> <br />
-                <button type="submit">Register</button>
-            </form>
         </div>        
         </>
     )
