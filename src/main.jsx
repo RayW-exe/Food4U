f27d9770c4f173f38c7b99613ceaaa579c1048cf
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-05-18 09:50:56 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter} from 'react-router'
+import {BrowserRouter} from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/Food4U'>
        <App />
-    </HashRouter>
+    </BrowserRouter  >
   </StrictMode>,
 )
