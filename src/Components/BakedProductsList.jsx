ecd7fce06d016f0842a3f65f4c1ff611a8056628
RayW.exe <ray.wekesa@student.moringaschool.com>
2026-05-18 09:56:23 +0300

    styling fixed

--- src/Components/BakedProductsList.jsx
+++ src/Components/BakedProductsList.jsx
@@ -7,7 +7,7 @@ function BakedProductsList ({bakedItems, search, setSearch}) {
     return(
         <>
          <div className="min-h-screen bg-linear-to-b from-yellow-300 via-yellow-500 to-orange-600 flex items-center justify-center px-4 py-8">
-            <div className="bg-black/40 backdrop-blur-md border-white/60 rounded-4xl shadow-2xl/35 p-6 sm:p-10 w-full max-w-screen-lg m-5">
+            <div className="bg-black/40 backdrop-blur-md border-white/60 rounded-4xl shadow-2xl/35 p-6 sm:p-10 w-full max-w-5xl m-5">
                 <Link to="/Recipes" className="bg-white/30 px-4 py-3 rounded-2xl hover:bg-white/40 mb-5 inline-block">Back to Recipes</Link>
                 <Search search={search} setSearch={setSearch}/>
                 <div className="grid gap-6">
